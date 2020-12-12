f00fb9a816ca0a370d44a7a4f2cae963e960808a
Andrew Liu <aliu.13119@gmail.com>
2020-12-11 16:20:26 -0800

    Added labelling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div id="output">
+      <div>
         <header></header>
         <div id="myerson">
           <Myerson />
